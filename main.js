273fd468b835b560dc7527b63c96f3c789f0d874
Yann Mulonda <yannmjl@gmail.com>
2019-04-01 12:48:11 -0500

    error on displaying xml content

--- main.js
+++ main.js
@@ -8,22 +8,91 @@ $(document).ready(function() {
 
 // Get the data we need through an AJAX call
 // Read and Process XML using jQuery Ajax
-$(document).ready(function() {
-  console.log(" inside jquery function");
+// $(document).ready(function() {
+//   console.log(" inside jquery function");
+
+//   $.ajax({
+//       type: "GET",
+//       dataType: "xml",
+//       // data: {
+//       //   'url': unescape('https: //rssonefeed.aws.stthomas.edu/feed?id=140&feedReverse=true&feedReverse=true'),
+//       //  'cacheTime': 1
+//       // },
+//       url: 'https://webutils.aws.stthomas.edu/rssutilities.do?url=https://rssonefeed.aws.stthomas.edu/feed?id=140&feedReverse=true&feedReverse=true',
+//       error: function(e) {
+//         console.log(e);
+//         console.log("fail");
+//       }
+//     })
+//     .done(function(xml) {
+//       $(xml).find('item').each(function() {
+//         var _Title = $(this).find('title').text();
+//         console.log(_Title);
+
+//         var _Description = $(this).find('description').text();
+//         console.log(_Description);
+
+//         var _pubDate = $(this).find('pubDate').date();
+//         console.log(_pubDate);
+//       });
+
+//     });
+//   console.log("end of ajax call");
+// });
+
+
+// Get the data we need through an AJAX call
+
+// Read and Process XML using jQuery Ajax
+
+$(document).ready(function () {
 
   $.ajax({
+
       type: "GET",
+
       dataType: "xml",
-      // data: {
-      //   'url': unescape('https: //rssonefeed.aws.stthomas.edu/feed?id=140&feedReverse=true&feedReverse=true'),
-      //  'cacheTime': 1
-      // },
-      url: 'https://webutils.aws.stthomas.edu/rssutilities.do?url=https://rssonefeed.aws.stthomas.edu/feed?id=140&feedReverse=true&feedReverse=true',
-      error: function(e) {
-        console.log(e);
-        console.log("fail");
-      }
+
+      data: {
+
+        'url': 'https: //rssonefeed.aws.stthomas.edu/feed?id=140&feedReverse=true&feedReverse=true',
+
+        'cacheTime': 1
+
+      },
+
+      url: 'https://webutils.aws.stthomas.edu/rssutilities.do',
+
+      // success: function(xml) {
+
+      //   $(xml).find('item').each(function() {
+
+      //       var _Title = $(this).find('title').text();
+
+      //       console.log(_Title);
+
+      
+
+      //       var _Description = $(this).find('description').text();
+
+      //       console.log(_Description);
+
+      
+
+      //       var _pubDate = $(this).find('pubDate').date();
+
+      //       console.log(_pubDate);
+
+      //     },
+
+      error: function () {
+
+        alert("An error occurred while processing XML file.");
+
+      },
+
     })
+
     .done(function(xml) {
       $(xml).find('item').each(function() {
         var _Title = $(this).find('title').text();
